d58a49b908d7d3659b12d04f71c8a6a270357387
Stewie12061 <nhut170501@gmail.com>
2024-01-19 10:17:18 +0700

    add jenkinsfile

--- UpdateDatabases.ps1
+++ UpdateDatabases.ps1
@@ -5,7 +5,7 @@ param(
     [string]$sqlPassword
 )
 
-$sqlServer = "Server=$server;Database=$database;User Id=sa;Password=$sqlPassword;"
+$sqlServer = "Server=$server;Database=$database;User Id=sa;Password=$sqlPassword;Trusted_Connection=False;"
 
 Get-ChildItem -Path $scriptFolder -Filter *.sql -Recurse | ForEach-Object {
     sqlcmd -S $sqlServer -U sa -P $sqlPassword -d $database -i $_.FullName
